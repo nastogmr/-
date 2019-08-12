cb5566e6acdd5d37065c5fa752c2a4da394a5346
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-08-12 21:53:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ var fs = require('fs');
 var client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Welcome Bro ${client.user.tag}!`);
+  console.log(`By NaSto ${client.user.tag}!`);
 });
-var prefix = '.'
+var prefix = '#'
 
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
@@ -14,39 +14,40 @@ client.on('message', message => {
   if (message.author.id !== "595653353783033857") return;
 
   
-  if (message.content.startsWith(prefix + 'wat')) {
+  if (message.content.startsWith(prefix + 'setwatch')) {
   client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`**NaSto , ${argresult} احبك يالي تقر :white_check_mark: **`)
+    message.channel.sendMessage(`Watch Now: **${argresult} احبك يا لي تقرا** `)
+} 
 
  
-  if (message.content.startsWith(prefix + 'lis')) {
+  if (message.content.startsWith(prefix + 'setlis')) {
   client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
-    message.channel.sendMessage(`**NaSto , ${argresult} احبك يالي تقر :white_check_mark: **`)
+    message.channel.sendMessage(`LISTENING Now: **${argresult}احبك يا لي تقرا** `)
 } 
 
 
 if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.sendMessage(`**NaSto , ${argresult} احبك يالي تقر **`)
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
 } 
 
 if (message.content.startsWith(prefix + 'setavatar')) {
   client.user.setAvatar(argresult);
-   message.channel.sendMessage(`تم التغير **${argresult}**`);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + 'st')) {
+if (message.content.startsWith(prefix + 'setT')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
-    message.channel.sendMessage(`**NaSto , ${argresult} احبك يالي تقر :white_check_mark: **`)
+    message.channel.sendMessage(`Streaming: **${argresult}احبك يا لي تقرا** `)
 } 
-if (message.content.startsWith(prefix + 'ply')) {
+if (message.content.startsWith(prefix + 'setgame')) {
   client.user.setGame(argresult);
      console.log('test' + argresult);
-    message.channel.sendMessage(`**NaSto , ${argresult} احبك يالي تقر:white_check_mark:  ** `)
+    message.channel.sendMessage(`Playing: **${argresult}احبك يا لي تقرا** `)
 } 
 
 
