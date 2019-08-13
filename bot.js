98c9742d4bd0b91e72faaeeb6365600784f6163c
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-08-13 14:32:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ if (message.content.startsWith(prefix + 'setavatar')) {
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
-if (message.content.startsWith(prefix + '..')) {
+if (message.content.startsWith(prefix + '-')) {
   client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(` **NaSto  ✅ Streaming ** -->  ** ${argresult} **  `)
